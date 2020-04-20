f37e269a34e0e5fc9d988752ae1c8161d4e3edf5
Injae Lee <ilee.injae@gmail.com>
2020-04-20 05:17:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,53 +14,6 @@ songs = [
 
 binding.pry
 
-def help 
-  input = gets.strip 
-  if input.to_s == "help"
-  puts "I accept the following commands:"
-  puts "- help : display this help message"
-  puts "- list: displays a list of songs you can play"
-  puts "- lets you choose a song to play"
-  puts "- exits this program"
-  end
-end 
 
-def list(songs)
-  i = 0 
-  while i < songs.length do 
-    puts "#{i+1}. songs[i]"
-    i += 1 
-  end
-end
-  
-def play(songs)
-  valid_response = songs numbers and name  
-  puts "Please enter a song name or number"
-  response = gets.strip 
-  
-  if response == songs.include?(songs)
-    "Playing song name "
-  else 
-    puts "Invalid input, please try again"
-  end
-end
-
-def exit_jukebox 
-  puts "Goodbye"
-end
-
-def run 
-  puts "Please enter a command:"
-  input = gets.strip 
-  if input.to_s == "help" 
-    do this 
-  elsif input.to_s == "list"
-    do list 
-  elsif input.to_s == "play"
-    do play 
-  elsif input.to_s == "exit"
-    do exit 
-   end
- end
  
   
