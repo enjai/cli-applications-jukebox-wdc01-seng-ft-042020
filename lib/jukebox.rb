0808380dcd4a7a43c0034add685d5b9d2baa7398
Injae Lee <ilee.injae@gmail.com>
2020-04-20 05:20:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,7 +16,7 @@ def helphelp
   puts "- help : display this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts "- exit :exits this program"
+  puts "- exit : exits this program"
 end 
 
 binding.pry
