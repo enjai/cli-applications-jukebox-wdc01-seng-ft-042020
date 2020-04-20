a213c8dac1ef8b887cabbac87bc2b48704430990
Injae Lee <ilee.injae@gmail.com>
2020-04-20 05:53:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,17 +32,17 @@ end
 end
 
 def play(songs)
-  valid_input = list 
-  puts "Please enter a song name or number"
-  input = gets.downcase.strip 
-  if (1..9).to_a.include?(input.to_i) 
-    puts "Playing #{songs[input.to_i-1]}"
-  elsif songs.include?(input)
-    puts "Playing #{input}"
-  else
+  puts "Please enter a song name or number:"
+  user_response = gets.downcase.chomp 
+  
+  if (1..9).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+    elsif songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else 
     puts "Invalid input, please try again"
-  end
-end
+  end 
+end 
 
 
   
