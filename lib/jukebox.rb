4e8e8c221190774d13fd58fda3b45ae782dc13dd
Injae Lee <ilee.injae@gmail.com>
2020-04-20 05:42:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,7 +36,7 @@ def play(songs)
   puts "Please enter a song name or number"
   input = gets.downcase.strip 
   if (1..9).to_a.include?(input.to_i) 
-    puts "Playing #{songs[i]} "
+    puts "Playing #{songs[input.to_i-1]} "
   
   
 
